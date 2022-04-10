2050a9c43e14fa6741333cd5237b8c2e2e2fedf6
Rasheed alwahbany <rasheedalwahbany@gmail.com>
2022-04-10 03:59:47 -0700

    Routing by react-route-dom 3

--- src/pages/routing/about.js
+++ src/pages/routing/about.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 const About = () => {
+    const {id}=useParams();
     return (
         <div>
-            About us
+            About us {id}
         </div>
     );
 }
--- src/pages/routing/index.js
+++ src/pages/routing/index.js
@@ -5,18 +5,19 @@ import Products from './products';
 import Error from './error';
 import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 const Indexs = () => {
+    
     return ( 
         <BrowserRouter>
             <div>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/about/1">About</Link></li>
                     <li><Link to="/products">Products</Link></li>
                 </ul>
             </div>
             <Routes>
             <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />}/>
+            <Route path='/about/:id' element={<About />}/>
             <Route path='/products' element={<Products />}/>
             <Route path='*' element={<Error />}/>
                 
